a541d8d66a72a6527688c8946ac074b972abe8c0
Naphat654 <113969092+Naphat654@users.noreply.github.com>
2022-11-24 16:14:30 +0700

    home

---
+++ home/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import View
+from django.http import JsonResponse
+from django import forms
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from django.db import connection
+
+from report.models import *
+import json
+
+# Create your views here.
+def index(request):
+    return render(request, 'base.html')
+
+def Home(request):
+    return render(request, 'home/Home.html')
+
+def recommended_menu(request):
+    return render(request, 'home/Recommended_Menu.html')
+
+def promotion(request):
+    return render(request, 'home/Promotion.html')

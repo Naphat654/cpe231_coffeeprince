b1211a30f2a5a823943c4f1077a54c675fb15f66
Naphat654 <Weeraphat.netprom@gmail.com>
2022-11-24 21:13:47 +0700

    แก้ไขหน้า order

--- order/admin.py
+++ order/admin.py
@@ -4,10 +4,7 @@ from django.contrib import admin
 from .models import Data
 from .models import Customer
 from .models import PaymentMethod
-<<<<<<< HEAD
 from .models import Menu
-=======
->>>>>>> 488b168e64e8633a9e3eb0323a892a521401c92c
 from .models import Stock
 from .models import OrderLineItem
 from .models import Orders
@@ -18,11 +15,8 @@ admin.site.register(Data)
 admin.site.register(Customer)
 admin.site.register(PaymentMethod)
 admin.site.register(Stock)
-<<<<<<< HEAD
 admin.site.register(Menu)
-=======
 admin.site.register(OrderLineItem)
 admin.site.register(Orders)
 admin.site.register(Sweet)
 admin.site.register(AdditionalItems)
->>>>>>> 488b168e64e8633a9e3eb0323a892a521401c92c
